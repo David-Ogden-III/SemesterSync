c360d79504f3fbb917dd72ecd954e61b28df8d2c
David Ogden <david-ogden699@hotmail.com>
2024-07-10 13:39:06 -0500

    Updated to support the deletion of terms and navigation

--- ViewModel/AllTermsViewModel.cs
+++ ViewModel/AllTermsViewModel.cs
@@ -2,13 +2,22 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using C971_Ogden.Database;
+using System.Diagnostics;
 
 namespace C971_Ogden.ViewModel;
 
 public class AllTermsViewModel : INotifyPropertyChanged
 {
-    private static SchoolDatabase Db { get; set; } = new();
-    // public ObservableCollection<Term> AllTerms { get; set; } = 
+    public AllTermsViewModel()
+    {
+        Classes = [];
+        LoadCommand = new Command(execute: async () => await LoadClasses());
+        DeleteTermCommand = new Command<ClassGroup>(execute: async (ClassGroup cg) => await DeleteTerm(cg));
+    }
+
+    public ObservableCollection<ClassGroup> Classes { get; set; }
+    public Command LoadCommand { get; }
+    public Command DeleteTermCommand { get; }
 
     public Command AddTermCommand { get; set; } = new(
         execute: async () =>
@@ -16,15 +25,55 @@ public class AllTermsViewModel : INotifyPropertyChanged
         await Shell.Current.GoToAsync(nameof(TermDetails));
     });
 
-    public Command<Term> EditTermCommand { get; set; } = new(
-        execute: async (Term selectedTerm) =>
+    public Command<ClassGroup> EditTermCommand { get; set; } = new(
+        execute: async (ClassGroup selectedCG) =>
     {
         await Shell.Current.GoToAsync(nameof(TermDetails),
-            new Dictionary<string,object>
+            new Dictionary<string, object>
             {
-                {"SelectedTerm", selectedTerm }
+                {"SelectedCG", selectedCG }
             });
     });
+
+    private async Task DeleteTerm(ClassGroup selectedCG)
+    {
+            Term selectedTerm = selectedCG.Term;
+            bool itemDeleted = await SchoolDatabase.DeleteItemAsync(selectedTerm);
+
+        foreach (Class classToDelete in selectedCG)
+        {
+            string className = classToDelete.ClassName;
+            bool classDeleted = await SchoolDatabase.DeleteItemAsync(classToDelete);
+            Debug.WriteLineIf(classDeleted, $"Deleted Class: {className}");
+        }
+
+        if (itemDeleted)
+            Classes.Remove(selectedCG);
+    }
+
+
+    private async Task LoadClasses()
+    {
+        Classes.Clear();
+        List<Term> allTermResults = (await SchoolDatabase.GetAllAsync<Term>()).ToList();
+        ObservableCollection<ClassGroup> cg = [];
+
+        foreach (Term term in allTermResults)
+        {            
+            List<TermSchedule> termSchedules = (await SchoolDatabase.GetFilteredListAsync<TermSchedule>((ts) => ts.TermId ==  term.Id)).ToList();
+
+            ObservableCollection<Class> classes = [];
+
+            foreach (TermSchedule termSchedule in termSchedules)
+            {
+                Class selectedClass = await SchoolDatabase.GetFilteredItemAsync<Class>((c) => c.Id == termSchedule.ClassId);
+                classes.Add(selectedClass);
+            }
+
+            Classes.Add(new ClassGroup (term, classes));
+        }
+
+    }
 
 
     public event PropertyChangedEventHandler? PropertyChanged;
