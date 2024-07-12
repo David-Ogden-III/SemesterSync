5b3b7f546a14861aed560fef6f02043f694a1707
David Ogden <david-ogden699@hotmail.com>
2024-07-12 10:16:11 -0500

    Setup ellipsis clicked action sheet and navigtion

--- ViewModel/ActiveTermViewModel.cs
+++ ViewModel/ActiveTermViewModel.cs
@@ -2,23 +2,27 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using C971_Ogden.Database;
+using C971_Ogden.Pages;
 
 namespace C971_Ogden.ViewModel;
 
 public class ActiveTermViewModel : INotifyPropertyChanged
 {
-    public ObservableCollection<Class> ActiveClasses { get; set; }
-    public Command LoadCommand { get; }
-
     public ActiveTermViewModel()
     {
-        ActiveClasses = [];
-        LoadCommand = new Command(execute: async () => await LoadActiveTermAsync());
         ActiveTerm = null;
         ActiveTermIsNotNull = false;
+
+        LoadCommand = new Command(execute: async () => await LoadActiveTermAsync());
+        EllipsisClickedCommand = new Command<Class>(execute: async (Class selectedClass) => await EllipsisClicked(selectedClass));
     }
 
-    // Fields and Properties
+    // Collections
+    public ObservableCollection<Class> ActiveClasses { get; set; } = [];
+    public Command LoadCommand { get; }
+
+
+    // Objects
     private Term? _activeTerm;
     public Term? ActiveTerm
     {
@@ -42,6 +46,21 @@ public class ActiveTermViewModel : INotifyPropertyChanged
     }
 
 
+    // Commands
+    public Command EllipsisClickedCommand { get; }
+
+    public Command<Class> DetailedViewCommand { get; set; } = new(
+        execute: async (Class selectedClass) =>
+        {
+            await Shell.Current.GoToAsync(nameof(DetailedClass),
+                new Dictionary<string, object>
+                {
+                {"SelectedClass", selectedClass }
+                });
+        });
+
+
+    // Command Definitions
     public async Task LoadActiveTermAsync()
     {
         var activeTerm = await SchoolDatabase.GetFilteredItemAsync<Term>((term) => term.StartDate < DateTime.Now && term.EndDate > DateTime.Now);
@@ -88,7 +107,24 @@ public class ActiveTermViewModel : INotifyPropertyChanged
         }
     }
 
+    private async Task EllipsisClicked(Class selectedClass)
+    {
+        string action = await Shell.Current.CurrentPage.DisplayActionSheet(selectedClass.ClassName, "Cancel", null, "Detailed View");
+        Debug.WriteLine("Action: " + action);
 
+        switch (action)
+        {
+            case "Detailed View":
+                DetailedViewCommand.Execute(selectedClass);
+                break;
+            default:
+                Debug.WriteLine("No Action Selected");
+                break;
+        }
+    }
+
+
+    // Helpers
     public event PropertyChangedEventHandler? PropertyChanged;
     void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
