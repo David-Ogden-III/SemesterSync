2a50a7ef97e0efb122c95d81734884853c3e7964
David Ogden <david-ogden699@hotmail.com>
2024-07-12 00:43:00 -0500

    Created new pages

---
+++ Pages/DetailedClass.xaml.cs
@@ -0,0 +1,9 @@
+namespace C971_Ogden.Pages;
+
+public partial class DetailedClass : ContentPage
+{
+	public DetailedClass()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pages/UpdateClass.xaml.cs
@@ -0,0 +1,9 @@
+namespace C971_Ogden.Pages;
+
+public partial class UpdateClass : ContentPage
+{
+	public UpdateClass()
+	{
+		InitializeComponent();
+	}
+}
