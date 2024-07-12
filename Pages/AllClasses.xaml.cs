1bb6e37ef2542b700f13c4d6678c4fa28594f1e3
David Ogden <david-ogden699@hotmail.com>
2024-07-12 00:43:32 -0500

    setup binding context for all classes

--- Pages/AllClasses.xaml.cs
+++ Pages/AllClasses.xaml.cs
@@ -1,9 +1,14 @@
+using C971_Ogden.ViewModel;
+using C971_Ogden.Database;
+using System.Diagnostics;
+
 namespace C971_Ogden.Pages;
 
 public partial class AllClasses : ContentPage
 {
-    public AllClasses()
+    public AllClasses(AllClassesViewModel vm)
     {
         InitializeComponent();
+        BindingContext = vm;
     }
 }
