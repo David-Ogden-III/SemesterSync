8f0c1b6097d9a41079b4fe639acb5140875deb2a
David Ogden <david-ogden699@hotmail.com>
2024-07-14 15:46:42 -0500

    added viewmodel binding context

--- Pages/UpdateClass.xaml.cs
+++ Pages/UpdateClass.xaml.cs
@@ -1,9 +1,12 @@
+using C971_Ogden.ViewModel;
+
 namespace C971_Ogden.Pages;
 
 public partial class UpdateClass : ContentPage
 {
-	public UpdateClass()
+	public UpdateClass(UpdateClassViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
