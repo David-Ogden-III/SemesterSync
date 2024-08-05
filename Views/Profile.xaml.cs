e32c66aa93f02f9646b82f789fc503fe82a3ebe5
David Ogden <david-ogden699@hotmail.com>
2024-08-04 22:23:24 -0500

    Added actionsheet functionality for logout and report functionalities

--- Views/Profile.xaml.cs
+++ Views/Profile.xaml.cs
@@ -1,4 +1,5 @@
 using SemesterSync.ViewModel;
+using System.Diagnostics;
 
 namespace SemesterSync.Views;
 
@@ -8,5 +9,21 @@ public partial class Profile : ContentPage
     {
         InitializeComponent();
         BindingContext = vm;
+    }
+
+    private async void Ellipsis_Clicked(object sender, EventArgs e)
+    {
+        string action = await DisplayActionSheet("Select", "Cancel", null, "Progress Reports", "Logout");
+
+        switch (action)
+        {
+            case "Progress Reports":
+                Debug.WriteLine("prog repo");
+                break;
+            case "Logout":
+                SecureStorage.Remove("ActiveUser");
+                await Shell.Current.GoToAsync($"//{nameof(Login)}");
+                break;
+        }
     }
 }
