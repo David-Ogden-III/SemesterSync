1ca8cc32182842af70d2f3a206bea3704eac617d
David Ogden <david-ogden699@hotmail.com>
2024-08-06 23:34:50 -0500

    Used for status reports

---
+++ ModelLibrary/ReportGroup.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ModelLibrary;
+
+public class ReportGroup(string status, ObservableCollection<Class> classes) : ObservableCollection<Class>(classes)
+{
+    public string Status { get; set; } = status;
+}
