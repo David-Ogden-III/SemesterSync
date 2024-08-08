75bb75be889734a949995ce5611e15a610f78179
David Ogden <david-ogden699@hotmail.com>
2024-08-07 22:54:44 -0500

    created but needs popupservice dependency injection

---
+++ SemesterSyncTest/AllClassesVMTest.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Core;
+using DataLibrary;
+using ModelLibrary;
+using ViewModelLibrary;
+
+namespace SemesterSyncTest;
+
+public class AllClassesVMTest
+{
+    //[Fact]
+    //public void FilterClasses_SearchParamNull_ClassesNotNull()
+    //{
+    //    PopupService stubPopupService = new();
+    //    AllClassesViewModel vm = new(stubPopupService)
+    //    {
+    //        SearchParams = String.Empty,
+    //        ClassesSourceOfTruth = [new Class(), new Class()]
+    //    };
+    //    vm.Classes.Clear();
+        
+
+    //    vm.FilterClasses();
+
+    //    Assert.NotEmpty(vm.Classes);
+    //}
+}
